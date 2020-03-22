ab5e557b73f6e039aef62caf48baa96fd70fe4f8
VisheshGavli <visheshgavli@gmail.com>
2020-03-22 20:17:20 +0800

    .

--- news_config.py
+++
@@ -1,9 +0,0 @@
-"""
-User settings for news.
-"""
-
-# Set the number of Articles
-headline_count = 10
-
-#Category
-st_categories = [("OCBC", "https://www.straitstimes.com/tags/ocbc", "OCBC_Singapore.txt")]
